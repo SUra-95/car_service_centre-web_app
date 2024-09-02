2fc1408af86a3a0b4c2632e330e696f538dbffdf
Suranjan De Silva <suranjandesilva1995@gmail.com>
2024-09-02 10:32:44 +0530

    Add Customer done

--- src/app/Livewire/Customers.php
+++ src/app/Livewire/Customers.php
@@ -11,23 +11,53 @@ class Customers extends Component
     use WithPagination;
 
     public $search;
+    public $customer;
+    public $NIC, $name, $email, $phone, $address;
     public $confirmingCustomerDeletion = false;
+    public $confirmingCustomerAddition = false;
+    public $password;
     protected $queryString = [
         'search' => ['except' => '']
+    ];
+    protected $rules = [
+        'NIC' => 'required|min:10|max:15',
+        'name' => 'required|min:3|max:50',
+        'email' => 'required|email',
+        'password' => 'required|min:8',
+        'phone' => 'required|regex:/^\d{10}$/',
+        'address' => 'required|string|max:255',
     ];
 
     public function confirmCustomerDeletion( $id ){
         $this->confirmingCustomerDeletion = $id;
     }
-
     public function deleteCustomer(Customer $customer){
+        dd('dwwwwwwwwwwwww');
         $customer->delete();
         $this->confirmingCustomerDeletion = false;
     }
     public function cancelDeleteModel(){
         $this->confirmingCustomerDeletion = false;
     }
-    
+    public function confirmCustomerAddition(){
+        $this->confirmingCustomerAddition = true;
+    }
+    public function cancelAddModel(){
+        $this->confirmingCustomerAddition = false;
+
+    }
+    public function saveCustomer(){
+        // dd('awaa');
+        $this->password = $this->generateRandomPassword();
+        $validated = $this->validate($this->rules);
+        Customer::create($validated);
+        $this->confirmingCustomerAddition = false;
+    }
+
+    protected function generateRandomPassword($length = 8)
+    {
+        return bin2hex(random_bytes($length / 2));
+    }
 
 
     public function render()
@@ -44,7 +74,7 @@ class Customers extends Component
 
             return view('livewire.customers', 
             [
-                'customers' => Customer::latest()
+                'customers' => Customer::query()
                     ->where('email', 'like', "%{$this->search}%")
                     ->paginate(2)
             ]
--- src/resources/views/livewire/customers.blade.php
+++ src/resources/views/livewire/customers.blade.php
@@ -1,4 +1,12 @@
 <div class="p-6 lg:p-8 bg-white border-b border-gray-200">
+    <div class="mt-8 text-2xl flex justify-between">
+        <div>Items</div>
+        <div class="mr-2">
+            <x-button wire:click="confirmCustomerAddition" class="bg-blue-800 hover:bg-blue-950">
+                {{ __('Add new Customer') }}
+            </x-button>
+        </div>
+    </div>
     <div class="mt-6">
         <div class="flex justify-between">
             <div class="p-2">
@@ -64,8 +72,54 @@
                 {{ __('Cancel') }}
             </x-secondary-button>
 
-            <x-danger-button class="ms-3" wire:click="deleteCustomer({{ $confirmingCustomerDeletion }})" wire:loading.attr="disabled">
+            <x-danger-button class="ms-3" wire:click="deleteCustomer({{ $confirmingCustomerDeletion }})"
+                wire:loading.attr="disabled">
                 {{ __('Delete') }}
+            </x-danger-button>
+        </x-slot>
+    </x-dialog-modal>
+
+
+    <x-dialog-modal wire:model.live="confirmingCustomerAddition">
+        <x-slot name="title">
+            {{ __('Add Customer') }}
+        </x-slot>
+
+        <x-slot name="content">
+            <div class="col-span-6 sm:col-span-4 mt-2">
+                <x-label for="NIC" value="{{ __('NIC') }}" />
+                <x-input id="NIC" name="NIC" type="text" class="mt-1 block w-full" wire:model="NIC" required />
+                <x-input-error for="NIC" class="mt-2" />
+            </div>
+            <div class="col-span-6 sm:col-span-4 mt-2">
+                <x-label for="name" value="{{ __('Name') }}" />
+                <x-input id="name" name="name" type="text" class="mt-1 block w-full" wire:model="name" required />
+                <x-input-error for="name" class="mt-2" />
+            </div>
+            <div class="col-span-6 sm:col-span-4 mt-2">
+                <x-label for="email" value="{{ __('Email') }}" />
+                <x-input id="email" name="email" type="text" class="mt-1 block w-full" wire:model="email" required />
+                <x-input-error for="email" class="mt-2" />
+            </div>
+            <div class="col-span-6 sm:col-span-4 mt-2">
+                <x-label for="phone" value="{{ __('Phone') }}" />
+                <x-input id="phone" name="phone" type="text" class="mt-1 block w-full" wire:model="phone" required />
+                <x-input-error for="phone" class="mt-2" />
+            </div>
+            <div class="col-span-6 sm:col-span-4 mt-2">
+                <x-label for="address" value="{{ __('Address') }}" />
+                <x-input id="address" name="address" type="text" class="mt-1 block w-full" wire:model="address" required />
+                <x-input-error for="address" class="mt-2" />
+            </div>
+        </x-slot>
+
+        <x-slot name="footer">
+            <x-secondary-button wire:click="cancelAddModel" wire:loading.attr="disabled">
+                {{ __('Cancel') }}
+            </x-secondary-button>
+
+            <x-danger-button class="ms-3" wire:click="saveCustomer" wire:loading.attr="disabled">
+                {{ __('Save') }}
             </x-danger-button>
         </x-slot>
     </x-dialog-modal>
